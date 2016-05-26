1bdef9343d09c58959747ad3092e0a050e85d385
Keith Bauson <kwbauson@gmail.com>
2016-05-26 16:08:21 -0400

    Initial materializing

--- webapp/src/app/faraday.component.ts
+++ webapp/src/app/faraday.component.ts
@@ -1,23 +1,43 @@
 import { Component } from '@angular/core';
-import { CourseComponent } from './+course';
-import { Routes , ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
-import { CoursesComponent } from './+courses';
+import { Router, Routes, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from '@angular/router';
 
-import { CourseService } from './shared';
+import { MD_CARD_DIRECTIVES } from '@angular2-material/card';
+import { MdButton } from '@angular2-material/button';
+import { MdToolbar } from '@angular2-material/toolbar';
+
+import {
+  CourseService,
+  CourseComponent,
+  CoursesComponent,
+} from './shared';
 
 @Component({
   moduleId: module.id,
   selector: 'faraday-app',
   templateUrl: 'faraday.component.html',
   styleUrls: ['faraday.component.css'],
-  directives: [ROUTER_DIRECTIVES],
+  directives: [
+    ROUTER_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MdButton,
+    MdToolbar,
+  ],
   providers: [ROUTER_PROVIDERS, CourseService]
 })
 @Routes([
   {path: '/course/:id', component: CourseComponent},
-  {path: '/courses', component: CoursesComponent},
-  {path: '*', component: CoursesComponent}
+  {path: '/course', component: CoursesComponent},
+  {path: '/', component: CoursesComponent}
 ])
 export class FaradayAppComponent {
+
+  constructor(
+    private router: Router) {
+  }
+
+  gotoCourses() {
+    this.router.navigate(['/course']);
+  }
+
   title = 'faraday works!';
 }
