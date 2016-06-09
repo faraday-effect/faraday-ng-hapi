1c6fc993f0ff2e84ec73249b8f2cb89635cc5924
Tom Nurkkala <tnurkkala@cse.taylor.edu>
2016-06-09 19:04:04 -0400

    Finish migration to Objection for test/sections
    This is way better than Bookshelf.

--- server/test/sections.js
+++ server/test/sections.js
@@ -22,11 +22,13 @@ lab.before((done) => {
 
 lab.experiment('/sections endpoint', () => {
 
+    let foundations_section_id = null;
+
     lab.beforeEach(done => {
         return Promise.all([
+            db.knex.raw('TRUNCATE person CASCADE'),
             db.knex.raw('TRUNCATE course CASCADE'),
-            db.knex.raw('TRUNCATE student CASCADE'),
-            db.knex.raw('TRUNCATE section CASCADE')
+            db.knex.raw('TRUNCATE term CASCADE')
         ]).then(results => {
             return Term.query().insertAndFetch({
                 name: 'Fall 2016',
@@ -34,9 +36,7 @@ lab.experiment('/sections endpoint', () => {
                 end_date: '2016-12-15'
             })
         }).then(term => {
-            console.log("TERM", term);
-            return Promise.all([
-                Course.query().insertWithRelated({
+            return Course.query().insertWithRelated({
                     number: '121',
                     title: 'Foundations of Computer Science',
                     offerings: [{
@@ -46,36 +46,35 @@ lab.experiment('/sections endpoint', () => {
                             {title: 'Section 2', reg_number: 'REG222'}
                         ]
                     }]
-                }),
-            ])
-        }).then(results => {
+                })
+        }).then(course => {
+            foundations_section_id = course.offerings[0].sections[0].id;
+
             return Promise.all([
-                Section.query().insert({id: 1, reg_number: 'REG111', title: 'Section 1'}),
-                Section.query().insert({id: 2, reg_number: 'REG222', title: 'Section 2'})
-            ])
-        }).then(results => {
-            return Promise.all([
-                Person.query().insert({
+                Person.query().insertAndFetch({
                     first_name: "Patty",
                     last_name: "O'Furniture",
                     email: 'patty@example.com',
                     password: 'pass'
+                }).then(person => {
+                    return person.$relatedQuery('student_sections').relate(foundations_section_id);
                 }),
                 Person.query().insert({
                     first_name: "Frank",
                     last_name: "Insense",
                     email: 'frank@example.com',
                     password: 'pass'
-                }),
+                })
             ])
         }).catch(err => {
-            console.log("ERROR", err)
+            console.log("ERROR", err);
         });
 
-        done();
+        // No need to invoke done();  According to documentation,
+        // you can return a promise instead.
     });
 
-    lab.test('There are 2 sections', (done) => {
+    lab.test('There are a known number of sections', (done) => {
         server.inject(
             {
                 method: 'GET',
@@ -89,34 +88,34 @@ lab.experiment('/sections endpoint', () => {
             })
     });
 
-    lab.test('Section 1 is the foundations class', (done) => {
+    lab.test('One of the sections is for Foundations of CS', (done) => {
         server.inject(
             {
                 method: 'GET',
-                url: '/sections/1',
+                url: `/sections/${foundations_section_id}`,
                 credentials: {}
             },
             (res) => {
                 expect(res.statusCode).to.equal(200);
                 const response = JSON.parse(res.payload);
-                expect(response.course.number).to.equal('121');
-                expect(response.course.title).to.equal('Foundations of Computer Science');
+                expect(response.offering.course.number).to.equal('121');
+                expect(response.offering.course.title).to.equal('Foundations of Computer Science');
                 done();
             })
     });
 
-    lab.test('Abram is the only student in section 1', (done) => {
+    lab.test('Patty is the only student in Foundations of CS', (done) => {
         server.inject(
             {
                 method: 'GET',
-                url: '/sections/1/students',
+                url: `/sections/${foundations_section_id}/students`,
                 credentials: {}
             },
             (res) => {
                 expect(res.statusCode).to.equal(200);
                 const response = JSON.parse(res.payload);
                 expect(response).to.have.length(1);
-                expect(response[0].first_name).to.equal('Abram');
+                expect(response[0].first_name).to.equal('Patty');
                 done();
             })
     });
