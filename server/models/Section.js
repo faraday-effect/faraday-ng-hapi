d2e7ac4b3ba0255d6db62056c57418952b844e81
Abram Stamper <abram.j.stamper@me.com>
2016-06-13 10:38:06 -0400

    added member model

--- server/models/Section.js
+++ server/models/Section.js
@@ -10,38 +10,14 @@ class Section extends Model {
         return {
             students: {
                 relation: Model.ManyToManyRelation,
-                modelClass: __dirname + '/Person',
+                modelClass: __dirname + '/User',
                 join: {
                     from: 'section.id',
                     through: {
                         from: 'student.section_id',
-                        to: 'student.person_id'
+                        to: 'student.user_id'
                     },
-                    to: 'person.id'
-                }
-            },
-            instructors: {
-                relation: Model.ManyToManyRelation,
-                modelClass: __dirname + '/Person',
-                join: {
-                    from: 'section.id',
-                    through: {
-                        from: 'instructors.section_id',
-                        to: 'instructors.person_id'
-                    },
-                    to: 'person.id'
-                }
-            },
-            teaching_assistant: {
-                relation: Model.ManyToManyRelation,
-                modelClass: __dirname + '/Person',
-                join: {
-                    from: 'section.id',
-                    through: {
-                        from: 'teaching_assistant.section_id',
-                        to: 'teaching_assistant.person_id'
-                    },
-                    to: 'person.id'
+                    to: 'user.id'
                 }
             },
             offering: {
---
+++ server/models/User.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const Model = require('objection').Model;
+const db = require('./../db')
+
+class User extends Model {
+    static get tableName() {
+        return 'user';
+    }
+
+    static get relationMappings() {
+        return {
+            member_sections: {
+                relation: Model.ManyToManyRelation,
+                modelClass: __dirname + '/Section',
+                join: {
+                    from: 'user.id',
+                    through: {
+                        from: 'member.user_id',
+                        to: 'section.section_id'
+                    },
+                    to: 'section.id'
+                },
+            }
+        }
+    }
+
+    fullName() {
+        return this.firstName + ' ' + this.lastName
+    }
+
+    stripPassword() {
+        delete this['password']
+        return this;
+    }
+}
+
+module.exports = User;
