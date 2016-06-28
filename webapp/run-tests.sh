b26df5463addecf9b2fc5a70bd4f160b1173ef07
Keith Bauson <kwbauson@gmail.com>
2016-06-28 15:26:13 -0400

    Start `ng serve` before running tests

--- webapp/run-tests.sh
+++ webapp/run-tests.sh
@@ -4,7 +4,8 @@ cd ../server
 npm install
 ./node_modules/.bin/knex migrate:latest
 node start.js &
-sleep 3
 cd ../webapp
 npm install
+./node_modules/.bin/ng serve &
+sleep 5
 ./node_modules/.bin/ng e2e
